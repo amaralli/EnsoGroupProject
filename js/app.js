44cc792d48c4ebff926962c7265bd549482a7e9d
Tim <timowland@gmail.com>
2014-12-06 18:05:37 -0800

    more changes

--- js/app.js
+++ js/app.js
@@ -24,22 +24,22 @@
 //    }
 //}
 //
-//
-//// aboutenso employee table js
-//angular.module('AddressBook', [])
-//    .controller('AddressController', function($scope) {
-//        $scope.employees = ensoEmployees;
-//        $scope.sortCol = 'lastName';
-//        $scope.sortBy = function(sortCol) {
-//            if( $scope.sortCol == sortCol) {
-//                $scope.sortReverse = !$scope.sortReverse;
-//            }else {
-//                $scope.sortCol = sortCol;
-//                $scope.sortReverse = false;
-//            }
-//            $scope.sortCol = sortCol;
-//        };
-//        $scope.isSortedBy = function(colName) {
-//            return colName ==$scope.sortCol;
-//        };
-//    });
+
+// aboutenso employee table js
+angular.module('AddressBook', [])
+   .controller('AddressController', function($scope) {
+       $scope.employees = ensoEmployees;
+       $scope.sortCol = 'lastName';
+       $scope.sortBy = function(sortCol) {
+           if( $scope.sortCol == sortCol) {
+               $scope.sortReverse = !$scope.sortReverse;
+           }else {
+               $scope.sortCol = sortCol;
+               $scope.sortReverse = false;
+           }
+           $scope.sortCol = sortCol;
+       };
+       $scope.isSortedBy = function(colName) {
+           return colName ==$scope.sortCol;
+       };
+   });
